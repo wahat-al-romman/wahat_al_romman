c895ec322c78f0f1b3eacae5a739ab366ea60a28
wahat_al_romman <wahatalromman.foodstuff@gmail.com>
2024-03-15 18:22:03 +0530

    connect local to remote

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -5,17 +5,25 @@ const Landing = () => {
     const [animate, setAnimate] = useState(false);
 
     useEffect(() => {
-        console.log("trig");
-        window.addEventListener("load", () => {
-            console.log("trig2");
+
+        const handleLoad = () => {
             setAnimate(true);
-        });
+        };
+        const timeoutId = setTimeout(() => {
+            handleLoad()
+          }, 2000);
+        document.addEventListener('load', handleLoad);
+
+        return () => {
+            clearTimeout(timeoutId);
+            document.removeEventListener('load', handleLoad);
+        };
     }, []);
     console.log(animate);
 
     return (
         <div className="landing fixed flex items-center justify-center w-screen h-screen z-999 bg-zinc-900 text-center">
-            <span className={`font-anton text-9xl bg-food1 bg-clip-text bg-fixed bg-center bg-cover transition-all duration-[2000ms] ${animate ? 'text-transparent text-[200px]' : 'text-white'}`}>WAHAT AL ROMMAN</span>
+            <span className={`font-anton absolute text-9xl bg-food1 text-transparent bg-clip-text bg-fixed bg-center bg-cover transition-all duration-[2000ms] ${animate ? ' text-[200px]' : 'text-white'}`}>WAHAT AL ROMMAN</span>
         </div>
     );
 };
